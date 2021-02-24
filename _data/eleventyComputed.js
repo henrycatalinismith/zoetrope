ad4d09fea4a47a21e6dade0fcb4e3243c0427755
Henry Catalini Smith <henry@catalinismith.se>
2021-02-24 19:48:20 +0100

    get it working

---
+++ _data/eleventyComputed.js
@@ -0,0 +1,87 @@
+const fs = require("fs-extra")
+const _ = require("lodash")
+
+const package = fs.readJsonSync(
+  `${process.env.DIR}/package.json`,
+  "utf-8"
+)
+
+const author = _.get(package, "author")
+const description = _.get(package, "description", "")
+const name = _.get(package, "name", "").replace(/^.+\//, "")
+const repository = _.get(package, "repository")
+
+const opengraph = []
+const twitter = []
+
+const url = process.env.COMMIT_REF
+  ? package.homepage.replace(/\/$/, "")
+  : ""
+
+opengraph.push({
+  property: "og:title",
+  content: name,
+})
+
+opengraph.push({
+  property: "og:url",
+  content: url,
+})
+
+opengraph.push({
+  property: "og:description",
+  content: data => data.description,
+})
+
+const image = _.find(
+  package.files,
+  f => f.match(/opengraph/)
+)
+
+if (image) {
+  opengraph.push({
+    property: "og:image",
+    content: `${url}/${image}`,
+  })
+
+  opengraph.push({
+    property: "og:image:alt",
+    content: description,
+  })
+
+  opengraph.push({
+    property: "og:image:height",
+    content: 630,
+  })
+
+  opengraph.push({
+    property: "og:image:height",
+    content: 1200,
+  })
+}
+
+twitter.push({
+  name: "twitter:card",
+  content: "summary_large_image",
+})
+
+twitter.push({
+  name: "twitter:text:title",
+  content: name,
+})
+
+twitter.push({
+  name: "twitter:description",
+  content: description,
+})
+
+module.exports = {
+  author,
+  description,
+  name,
+  opengraph,
+  repository,
+  twitter,
+  url,
+}
+
