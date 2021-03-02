770cafe069c5cd8bb7bf3372725da7e8e92a9602
Henry Catalini Smith <henry@catalinismith.com>
2021-03-02 21:03:37 +0100

    tonnes of code churn here idk

--- cli/.eleventy.js
+++ cli/.eleventy.js
@@ -3,9 +3,10 @@ const createHtmlElement = require("create-html-element")
 const fs = require("fs-extra")
 const htmlmin = require("html-minifier")
 const path = require("path")
-const sass = require("sass")
 const shimmer = require("shimmer")
 const _ = require("lodash")
+
+const sass = require("../lib/sass")
 
 const package = fs.readJsonSync(
   `${process.env.DIR}/package.json`,
@@ -18,47 +19,15 @@ const files = _.get(package, "files", [])
 const mainPath = path.resolve(cwd, package.main)
 const mainBare = path.basename(mainPath, ".scss")
 
-const functions = {}
-
-functions['html($element, $props: (), $children: "")'] = function(element, props, children) {
-  const name = new sass.types.String(element.getValue()).getValue()
-
-  const attributes = {}
-  for (let i = 0; i < props.getLength(); i++) {
-    const key = new sass.types.String(props.getKey(i).getValue()).getValue()
-    const value = new sass.types.String(props.getValue(i).getValue()).getValue()
-    attributes[key] = value
-  }
-
-  const html = children.getValue()
-
-  return new sass.types.String(
-    createHtmlElement({
-      name,
-      attributes,
-      html,
-    })
-  )
-}
-
-functions['encode-uri($str)'] = function(str) {
-  return new sass.types.String(encodeURIComponent(str))
-}
-
 function compile() {
-  const target = `${process.env.DIR}/_site/${mainBare}-${version}.css`
-  const hrstart = process.hrtime()
-  const result = sass.renderSync({ file: mainPath, functions })
-  const hrend = process.hrtime()
-  console.log(`✨ ${mainBare}.css ${Math.floor(hrend[1] / 1000000)}ms`)
-  fs.ensureDirSync(`${process.env.DIR}/_site`)
-  fs.writeFileSync(target, result.css)
-}
+  sass.compile(
+    mainPath,
+    `${process.env.DIR}/_site/${mainBare}-${version}.css`
+  )
+)
 
 module.exports = function(eleventyConfig) {
   fs.emptyDirSync(`${process.env.DIR}/_site`)
-
-  compile()
 
   eleventyConfig.addPassthroughCopy(
     _.zipObject(
@@ -68,7 +37,6 @@ module.exports = function(eleventyConfig) {
   )
 
   setImmediate(function() {
-    let initialized = false
     const Eleventy = require("@11ty/eleventy/src/Eleventy.js")
     if (process.argv.includes("--serve")) {
       shimmer.wrap(Eleventy.prototype, "finish", function(orig) {
@@ -89,6 +57,8 @@ module.exports = function(eleventyConfig) {
       })
     }
   })
+
+  compile()
 
   eleventyConfig.addTransform(
     "htmlmin",
