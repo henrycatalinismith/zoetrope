241aacd37ea348002ce4aa028a4e739bafacb686
Henry Catalini Smith <henry@catalinismith.se>
2024-03-20 14:20:10 +0100

    Version 6 with extra server options + less metadata options

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,11 +14,25 @@ export default defineConfig({
       },
       testMatch: "red.spec.ts",
     },
+    {
+      name: "metadata",
+      use: {
+        ...devices["Desktop Chrome"],
+        baseURL: "http://127.0.0.1:8081",
+      },
+      testMatch: "metadata.spec.ts",
+    },
   ],
   webServer: [
     {
-      command: "node zoetrope.js serve red.scss",
+      command:
+        "node zoetrope.js --entrypoint red.html --port 8080 serve red.scss",
       url: "http://127.0.0.1:8080",
+    },
+    {
+      command:
+        "node zoetrope.js --entrypoint metadata.html --skipMenu false --port 8081 serve metadata.scss",
+      url: "http://127.0.0.1:8081",
     },
   ],
 });
--- red.spec.ts
+++ red.spec.ts
@@ -1,10 +1,11 @@
 const { test, expect, beforeEach } = require("@playwright/test");
 
 test("red background", async ({ page }) => {
+  await page.goto("/red.html");
+
   const body = page.locator("body");
   const loading = page.locator("[aria-label='loading']");
 
-  await page.goto("/");
   await loading.waitFor({ state: "visible" });
   await loading.waitFor({ state: "hidden" });
   const backgroundColor = await body.evaluate((el) => {
